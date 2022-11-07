a0549e3a3d122d16fe1b90a58f9472c8d57e58e6
Drew Miller <drew.miller@mrjaffesclass.com>
2022-11-07 11:34:23 -0800

    Redone

--- Dice/Dice.java
+++ Dice/Dice.java
@@ -1,25 +1,19 @@
 public class Dice
 {
-  private int value;
-  private int[] dice;
-  private int diceNumber;
-  public Dice(int diceNumber) {
-      this.diceNumber = diceNumber;
-      this.dice = new int[diceNumber];
-      this.roll();
+  Die6 die1;
+  Die6 die2;
+  public Dice() {
+      die1 = new Die6();
+      die2 = new Die6();
   }
-  public int[] getValue() {
-      return dice;
+  public int getValue() {
+      return die1.getValue() + die2.getValue();
   }
   public void roll() {
-      int i = 0;
-        while (i < this.diceNumber) {
-        this.value = (int)(Math.random() * 6) + 1;
-        this.dice[i] = value;
-        i++;
-      }
+      die1.roll();
+      die2.roll();
   }
-  public int[] rollAndGetValue() {
+  public int rollAndGetValue() {
       this.roll();
       return getValue();
   }
